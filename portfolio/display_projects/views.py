a33fb09593d5f6f9c7233f4a934b401b16054f0a
pasaunders <kaorti@gmail.com>
2017-06-12 20:28:56 -0700

    added portfolio display app and built very basic view for meal planner

--- portfolio/display_projects/views.py
+++ portfolio/display_projects/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at my meal planning index")
+    return HttpResponse("Placeholder")
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': '5432',
+        'TEST': {
+            'NAME': os.environ.get('TEST_PORTFOLIO', '')
+        }
+
     }
 }
 
