a3a06594ee39a25bbec45fa56aeb7d7dd6c8bdfa
pasaunders <kaorti@gmail.com>
2017-06-19 10:46:55 -0700

    made a new secret key and moved it to an enviornment variable

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3xuor@bsrl0-@mp7imse(8jj5*ve8#08sl_3b9p_w!v!0(!u_7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'bootstrap3',
+
     'meal_planner',
     'display_projects',
 ]
