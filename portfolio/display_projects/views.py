4f1bb5c43e777ece498e624d1ad6e3bcce271427
pasaunders <kaorti@gmail.com>
2017-06-20 11:21:04 -0700

    initial template, simple home view

--- portfolio/display_projects/views.py
+++ portfolio/display_projects/views.py
@@ -1,6 +1,12 @@
+"""Views for displaying my different programming projects."""
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Placeholder")
+
+
+def home_view(request):
+    """Home page view."""
+    context = {'example': 'example_context'}
+    return render(request, 'display_projects/home.html', context=context)
